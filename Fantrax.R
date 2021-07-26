1990f91849ce43df338ad2c7e5c2c2bfbf4cb953
jonathonkearney <Jonathon.kearney@gmail.com>
2021-07-26 23:26:21 +1200

    Add files via upload

---
+++ Fantrax.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(tidyverse)
+
+setwd("C:/Users/Joe/Documents/R")
+
+FS <- read.csv("FS.csv", header = TRUE)
+FT <- read.csv("FT.csv", header = TRUE)
+
+df <- merge(x = FT, y = FS)
+
+minMins <- 180
+
+#remove comma from data$Min and AP and convert to numeric 
+df$Min <- as.numeric(gsub("\\,", "", df$Min))
+df$Min <- as.numeric(as.character(df$Min))
+df$AP <- as.numeric(gsub("\\,", "", df$AP))
+df$AP <- as.numeric(as.character(df$AP))
+
+#remove all players with less than a certain amount of mins
+df <- subset(df, Min > minMins)
+
+for(i in 1:ncol(df)){
+  x <- colnames(df)[i]
+  if(x != "ID" && x != "Player" && x != "Team" && x != "Position"
+     && x != "Rk" && x != "Status" && x != "Opponent"
+     && x != "GP" && x != "PC" && x != "Min"){
+    colnames(df)[i] <- paste(colnames(df)[i], ".90", sep="" )
+  }
+}
+
+#Make most scores now be based of 90 mins
+for(i in 1:ncol(df)) { 
+  if(endsWith(colnames(df)[i], '90')){
+    df[,i] <- round((df[,i] / df$Min)*90, digits = 2)
+  }
+}
+
+ggplot(df, aes(G.90, A.90, colour = Position)) +
+  geom_point() + # Show dots
+  geom_text(
+    aes(label = Player),
+    check_overlap = T,
+    adj = -0.1
+  )
