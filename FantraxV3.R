f6cf1e5c6055b945f6f532863e931f3c62f8a863
jonathonkearney <Jonathon.kearney@gmail.com>
2022-09-13 14:54:44 +1200

    Add files via upload

--- FantraxV3.R
+++ FantraxV3.R
@@ -7,7 +7,7 @@ library(stringi)
 library(shinyWidgets)
 library(forecast)
 
-# **************************************************
+# *********************** LOAD & CLEAN DATA ***************************
 rm(list = ls())
 
 setwd("C:/Users/OEM/OneDrive/Documents/R/Fantrax/FantraxV3")
@@ -23,12 +23,7 @@ gws <- list(
 )
 
 #create an overall dataframes
-overall <- merge(x = read.csv("FT.csv", header = TRUE), y = read.csv("FS.csv", header = TRUE))
-
-#remove all players with less than a certain amount of mins. 
-#Don't do this for gws because some gws will have the player and some wont
-minMins <- 10
-overall <- subset(overall, Min > minMins)
+# OLDOverall <- merge(x = read.csv("FT.csv", header = TRUE), y = read.csv("FS.csv", header = TRUE))
 
 #Statuses
 statuses <- c("W (Mon)", "W (Tue)", "W (Wed)", "W (Thu)", "W (Fri)", "W (Sat)", "W (Sun)", "FA")
@@ -38,10 +33,8 @@ gws <- lapply(gws, function(x) mutate(x, Min = as.numeric(gsub("\\,", "", Min)))
 gws <- lapply(gws, function(x) mutate(x, Min = as.numeric(as.character(Min))))
 gws <- lapply(gws, function(x) mutate(x, AP = as.numeric(gsub("\\,", "", AP))))
 gws <- lapply(gws, function(x) mutate(x, AP = as.numeric(as.character(AP))))
-overall$Min <- as.numeric(gsub("\\,", "", overall$Min))
-overall$Min <- as.numeric(as.character(overall$Min))
-overall$AP <- as.numeric(gsub("\\,", "", overall$AP))
-overall$AP <- as.numeric(as.character(overall$AP))
+
+# *********************** ADD NEW COLUMNS ***************************
 
 #new columns
 gws <- lapply(gws, function(x) mutate(x, TkWAndIntAndCLR = TkW + Int + CLR))
@@ -49,66 +42,78 @@ gws <- lapply(gws, function(x) mutate(x, SOTAndKP = SOT + KP))
 gws <- lapply(gws, function(x) mutate(x, Min.GP = round((Min / GP) ,2)))
 gws <- lapply(gws, function(x) mutate(x, CoSMinusDIS = CoS - DIS))
 gws <- lapply(gws, function(x) mutate(x, SOTMinusG = SOT - G))
-gws <- lapply(gws, function(x) mutate(x, KPMinus = KP - A))
-overall <- mutate(overall, GSPercentage = round((GS / max(overall$GP)),2))
-overall <- mutate(overall, GPPercentage = round((GP / max(overall$GP)),2))
-overall <- mutate(overall, TkWAndIntAndCLR = TkW + Int + CLR)
-overall <- mutate(overall, SOTAndKP = SOT + KP)
-overall <- mutate(overall, Min.GP = round((Min / GP) ,2))
-overall <- mutate(overall, CoSMinusDIS = CoS - DIS)
-overall <- mutate(overall, SOTMinusG = SOT - G)
-overall <- mutate(overall, KPMinus = KP - A)
+gws <- lapply(gws, function(x) mutate(x, KPMinusA = KP - A))
 
-
-#Dynamically create last5 columns
-last5Columns <-  c("FPts", "G", "A", "Pts", "S", "SOT", "YC", "RC", "A2","KP",
-                   "AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int", "PKG",
-                   "CLR", "CoS", "AER", "PKM", "OG", "GAD", "CSD", "CSM",         
-                   "A", "FC", "FS", "DPt", "Off", "PKG", "CS", "TkWAndIntAndCLR",
-                   "SOTAndKP", "Min.GP", "CoSMinusDIS", "SOTMinusG", "KPMinus")
-
-for (i in last5Columns) {
-  #.l5
-}
-
-#Dynamically create .90 columns
+#Dynamically create .90 columns to gws
 per90Columns <-  c("FPts", "G", "A", "Pts", "S", "SOT", "YC", "RC", "A2","KP",
-"AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int", "PKG",
-"CLR", "CoS", "AER", "PKM", "OG", "GAD", "CSD", "CSM",         
-"A", "FC", "FS", "DPt", "Off", "PKG", "CS",  "TkWAndIntAndCLR",
-"SOTAndKP", "Min.GP", "CoSMinusDIS", "SOTMinusG", "KPMinus")
+"AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int",
+"CLR", "CoS", "AER", "PKM", "OG", "GAD", "CSD", "CSM",
+"A", "FC", "FS", "DPt", "Off", "CS",  "TkWAndIntAndCLR",
+"SOTAndKP", "CoSMinusDIS", "SOTMinusG", "KPMinusA")
 
 for (i in per90Columns) {
   gws <- lapply(gws, function(x) mutate(x, "{i}.90" := round(((x[[i]] / Min)*90),2)))
-  overall <- mutate(overall, "{i}.90" := round(((overall[[i]] / Min)*90),2))
 }
 
-#Potential
-gws <- lapply(gws, function(x) mutate(x, Potential = round((FPts.90 / FP.G),2)))
-overall <- mutate(overall, Potential = round((FPts.90 / FP.G),2))
+# *********************** CREATE OVERALL AND LAST5 DATAFRAMES ***************************
 
-#When a player doesnt play it gives 0's for everything and those 0's are counted in the SD, which is not ideal
-#Remove the row if they didnt play e.g. Min == 0
-gws <- lapply(gws, function(x) subset(x, Min != 0))
+#Test these by checking if sum() is the same
+
+#Use the latest GW as a starting template
+template <- as.data.frame(tail(gws, n=1))
+template <- select(template, c(ID, Player, Team, Position, Status, Opponent))
+overall <- template
+last5 <- template
+
+sumColumns <-  c("Min", "FPts", "GP", "GS", "G", "A", "Pts", "S", "SOT", "YC", "RC", "A2","KP",
+                 "AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int",
+                 "CLR", "CoS", "AER", "PKM", "OG", "GAD", "CSD", "CSM",         
+                 "FC", "FS", "DPt", "Off", "CS",  "TkWAndIntAndCLR",
+                 "SOTAndKP", "CoSMinusDIS", "SOTMinusG", "KPMinusA")
+
+
+for (i in sumColumns) {
+  overall <- left_join(overall, bind_rows(gws) %>% group_by(Player) %>% summarise("{i}" := sum(get(i))), by = "Player")
+  last5 <- left_join(last5, bind_rows(tail(gws, n=5)) %>% group_by(Player) %>% summarise("{i}" := sum(get(i))), by = "Player")
+}
+
+#Can't average out the gws .90 columns because that is mathmatically incorrect.
+for (i in per90Columns) {
+  overall <- mutate(overall, "{i}.90" := round(((get(i) / Min)*90),2))
+  last5 <- mutate(last5, "{i}.90" := round(((get(i) / Min)*90),2))
+}
+
+#Min.GP
+overall <- mutate(overall, Min.GP = round((Min / GP),2))
+last5 <- mutate(last5, Min.GP = round((Min / GP),2))
+#FP.G
+overall <- mutate(overall, FP.G = round((FPts / GP),2))
+last5 <- mutate(last5, FP.G = round((FPts / GP),2))
+
+# *********************** ADD SD TO DATAFRAMES ***************************
+
+#Remove the row if they didnt play e.g. Min == 0 (because 0's mess up SD)
+gwsMinus0Min <- lapply(gws, function(x) subset(x, Min != 0))
+
 
 #Dynamically create SD columns
-SDColumns <-  c("Min", "FPts", "FP.G", "G", "A", "Pts", "S", "SOT", "FC", "FS", "YC",
-                "RC", "DPt", "Off", "PKG", "CS", "A2", "KP",
-                "AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int", "PKG",
+SDColumns <-  c("Min", "Min.GP", "FPts", "FP.G", "G", "A", "Pts", "S", "SOT", "FC", "FS", "YC",
+                "RC", "DPt", "Off", "CS", "A2", "KP",
+                "AT", "TkW", "DIS", "ErG", "AP", "SFTP", "ACNC", "Int",
                 "CLR", "CoS", "AER", "PKM", "OG", "GAD", "CSD", "CSM",         
                 "TkWAndIntAndCLR", "SOTAndKP", 
                 "FPts.90", "G.90", "A.90", "Pts.90", "S.90", "SOT.90", "FC.90", "FS.90", "YC.90",
                 "RC.90", "DPt.90", "Off.90", "CS.90", "A2.90", "KP.90",
-                "AT.90", "TkW.90", "DIS.90", "ErG.90", "AP.90", "SFTP.90", "ACNC.90", "Int.90", "PKG.90",
+                "AT.90", "TkW.90", "DIS.90", "ErG.90", "AP.90", "SFTP.90", "ACNC.90", "Int.90",
                 "CLR.90", "CoS.90", "AER.90", "PKM.90", "OG.90", "GAD.90", "CSD.90", "CSM.90",         
                 "TkWAndIntAndCLR", "SOTAndKP")
 
 for (i in SDColumns) {
-  overall <- left_join(overall, bind_rows(gws) %>% group_by(Player) %>% summarise("{i}.SD" := sd(get(i), na.rm = TRUE)), by = "Player")
+  overall <- left_join(overall, bind_rows(gwsMinus0Min) %>% group_by(Player) %>% summarise("{i}.SD" := sd(get(i), na.rm = TRUE)), by = "Player")
+  last5 <- left_join(last5, bind_rows(tail(gwsMinus0Min, n=5)) %>% group_by(Player) %>% summarise("{i}.SD" := sd(get(i), na.rm = TRUE)), by = "Player")
 }
 
-#create the LQ (lower Quartile) columns
-overall <- left_join(overall, bind_rows(gws) %>% group_by(Player) %>% summarise(FP.G.LQ = quantile(FP.G, prob = c(.25), na.rm = TRUE)), by = "Player")
+# *********************** ADD TOP10 TO DATAFRAMES ***************************
 
 #Mark the top 10 players for each Status by their regular FP.G score
 top10 <- subset(overall, !(overall$Status %in% statuses))
@@ -121,8 +126,18 @@ top10 <- subset(top10, select = c(Player, Top10))
 overall <- full_join(overall, top10, by = "Player")
 overall$Top10 <- ifelse(is.na(overall$Top10), 0, overall$Top10)
 
+#Mark the top 10 players for each Status by their regular FP.G score
+top10 <- subset(last5, !(last5$Status %in% statuses))
+top10 <- top10 %>%                                      
+  arrange(desc(FP.G)) %>% 
+  group_by(Status) %>%
+  slice(1:10)
+top10$Top10 <- 1
+top10 <- subset(top10, select = c(Player, Top10))
+last5 <- full_join(last5, top10, by = "Player")
+last5$Top10 <- ifelse(is.na(last5$Top10), 0, last5$Top10)
 
-# **************************************************
+# ****************************************************************************************************
 
 ui <- fluidPage(
   
@@ -141,11 +156,11 @@ ui <- fluidPage(
                           selectInput("pPosition","Choose a Position", choices = c("All", "D", "M", "F"), selected = "All"),
                           selectInput("pYAxis","Choose the Y Axis", choices = sort(names(overall)), selected = "FP.G"),
                           selectInput("pXAxis","Choose the X Axis", choices = sort(names(overall)), selected = "FP.G.SD"),
-                          sliderInput("pMinMinsPerGP", "Minimum Minutes Per GP", min = min(overall$Min.GP), max = max(overall$Min.GP), value = min(overall$Min.GP)),
-                          sliderInput("pMinMins", "Minimum Total Minutes", min = min(overall$Min), max = max(overall$Min), value = min(overall$Min)),
-                          sliderInput("pMinFPts.90", "Minimum FPts per 90", min = min(overall$FPts.90), max = max(overall$FPts.90), value = min(overall$FPts.90)),
-                          sliderInput("pPCofGamesStarted", "% of Games Started", min = min(overall$GSPercentage), max = max(overall$GSPercentage), value = min(overall$GSPercentage)),
+                          sliderInput("pMinMinsPerGP", "Minimum Minutes Per GP", min = min(overall$Min.GP, na.rm = TRUE), max = max(overall$Min.GP, na.rm = TRUE), value = min(overall$Min.GP, na.rm = TRUE)),
+                          sliderInput("pMinMins", "Minimum Total Minutes", min = min(overall$Min, na.rm = TRUE), max = max(overall$Min, na.rm = TRUE), value = min(10, na.rm = TRUE)),
+                          sliderInput("pMinFPts.90", "Minimum FPts per 90", min = min(overall$FPts.90, na.rm = TRUE), max = max(overall$FPts.90, na.rm = TRUE), value = min(overall$FPts.90, na.rm = TRUE)),
                           checkboxInput("pAddLines", "Add Lines", value = FALSE, width = NULL),
+                          radioButtons("pLast5", "Overall or Last 5", choices = list("Overall" = 1, "Last 5" = 2),selected = 1),
                           checkboxInput("pTop10", "Top 10 Only", value = FALSE, width = NULL)
                           
                         ),
@@ -165,10 +180,10 @@ ui <- fluidPage(
                           selectInput("tTeam","Choose a team", choices = c("All",unique(overall$Team)), selected = "All"),
                           selectInput("tStatus","Choose a Status", choices = c("All", "All Available", "All Taken", unique(overall$Status), "Waiver"), selected = "All"),
                           selectInput("tPosition","Choose a Position", choices = c("All", "D", "M", "F"), selected = "All"),
-                          sliderInput("tMinMinsPerGP", "Minimum Minutes Per GP", min = min(overall$Min.GP), max = max(overall$Min.GP), value = min(overall$Min.GP)),
-                          sliderInput("tMinMins", "Minimum Total Minutes", min = min(overall$Min), max = max(overall$Min), value = min(overall$Min)),
+                          sliderInput("tMinMinsPerGP", "Minimum Minutes Per GP", min = min(overall$Min.GP, na.rm = TRUE), max = max(overall$Min.GP, na.rm = TRUE), value = min(overall$Min.GP, na.rm = TRUE)),
+                          sliderInput("tMinMins", "Minimum Total Minutes", min = min(overall$Min, na.rm = TRUE), max = max(overall$Min, na.rm = TRUE), value = min(overall$Min, na.rm = TRUE)),
                           pickerInput("tPicker", "Columns", choices = sort(names(overall)), options = list(`actions-box` = TRUE), selected=NULL, multiple=TRUE),
-                          sliderInput("tPCofGamesStarted", "% of Games Started", min = min(overall$GSPercentage), max = max(overall$GSPercentage), value = min(overall$GSPercentage)),
+                          radioButtons("tLast5", "Overall or Last 5", choices = list("Overall" = 1, "Last 5" = 2),selected = 1),
                         ),
                         
                         mainPanel(
@@ -186,6 +201,7 @@ ui <- fluidPage(
                           selectInput("bYAxis","Choose the Y Axis", choices = sort(names(overall)), selected = "FP.G.SD"),
                           selectInput("bXAxis","Choose the X Axis", choices = c("Status", "Team"), selected = "Status"),
                           selectInput("bPlotType","Plot Type", choices = c("Box", "Violin"), selected = "Box"),
+                          radioButtons("bLast5", "Overall or Last 5", choices = list("Overall" = 1, "Last 5" = 2),selected = 1),
                           checkboxInput("bTop10", "Top 10 Only", value = FALSE, width = NULL)
                         ),
                         
@@ -200,11 +216,14 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$plot <- renderPlot({
-    df_temp <- overall
+    if (input$pLast5 == 1) {
+      df_temp <- overall
+    }else{
+      df_temp <- last5
+    }
     df_temp <- filter(df_temp, Min.GP >= input$pMinMinsPerGP)
     df_temp <- filter(df_temp, Min >= input$pMinMins)
     df_temp <- filter(df_temp, FPts.90 >= input$pMinFPts.90)
-    df_temp <- filter(df_temp, GSPercentage >= input$pPCofGamesStarted)
     if (input$pTop10 == TRUE) {
       df_temp <- subset(df_temp, Top10 == 1)
     }
@@ -258,12 +277,15 @@ server <- function(input, output) {
   
   
   output$table = DT::renderDataTable({
-    df_temp <- overall
+    if (input$tLast5 == 1) {
+      df_temp <- overall
+    }else{
+      df_temp <- last5
+    }
     df_temp <- filter(df_temp, Min.GP >= input$tMinMinsPerGP)
     df_temp <- filter(df_temp, Min >= input$tMinMins)
-    df_temp <- filter(df_temp, df_temp$GSPercentage >= input$tPCofGamesStarted)
     
-    columns <- c("Player", "Team", "Status", "Position", "FP.G", "FP.G.SD", "FPts.90", "G", "A", "GSPercentage")
+    columns <- c("Player", "Team", "Status", "Position", "FP.G", "FP.G.SD", "FPts.90", "G", "A")
     columns <- append(columns, input$tPicker)
     
     df_temp <- df_temp[, which((names(df_temp) %in% columns)==TRUE)]
@@ -301,8 +323,11 @@ server <- function(input, output) {
   
   output$box <- renderPlot({
     
-    df_temp <- overall
-    
+    if (input$bLast5 == 1) {
+      df_temp <- overall
+    }else{
+      df_temp <- last5
+    }
     #If a player has only played one game then they wil have NAs for all their SD columns
     #This messes up box plots, specifically their order
     #If input$bYAxis ends with "SD" then filter out all NAs
